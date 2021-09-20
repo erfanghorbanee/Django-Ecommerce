a4f7bf690c8284802f6b288d9421ea7e6202d762
erfanghorbanee <erfanghorbanee@gmail.com>
2021-09-20 13:24:44 +0430

    Edit profile handled by ajax.

--- static/js/profile.js
+++ static/js/profile.js
@@ -5,3 +5,58 @@ function handleEdit() {
 
     return false;
 }
+
+function handleSave() {
+    document.getElementById("myFieldset").disabled = true;
+    document.getElementById('edit').hidden = false;
+    document.getElementById('save').hidden = true;
+
+    return false;
+}
+
+$(function() {
+   $.ajaxSetup({
+       headers: {
+         "X-CSRFToken": $('[name=csrfmiddlewaretoken]').val()
+       }
+   })
+});
+
+function edit_detail() {
+    console.log("edit is working!") // sanity check
+
+    $.ajax({
+        url: "/rest-auth/user/", // the endpoint
+        type: "PUT", // http method
+        data: {
+            email: $('#id_email').val(), // data sent with the post request
+            phone: $('#id_phone').val(),
+            first_name: $('#id_first_name').val(),
+            last_name: $('#id_last_name').val(),
+            gender: $('#id_gender').val(),
+        },
+
+        // handle a successful response
+        success: function (json) {
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+            alert("اطلاعات شما با موفقیت ویرایش شد.");
+        },
+
+        // handle a non-successful response
+        error: function (xhr) {
+
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+            alert(xhr.responseText);
+            // $("#talk").prepend("<li><strong>" + xhr.responseText + "</strong></li>");
+        }
+    });
+
+}
+
+$('#details').on('submit', function(event){
+    event.preventDefault();
+    console.log("form submitted!")  // sanity check
+    edit_detail();
+    handleSave();
+});

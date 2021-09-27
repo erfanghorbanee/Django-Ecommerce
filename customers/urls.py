3b89e0b26bcd32daf36be86e65b3f388f5b44bca
erfanghorbanee <erfanghorbanee@gmail.com>
2021-09-27 18:05:38 +0330

    Orders & Recent Orders page added.

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import profile_view
+from .views import profile_view, orders_history_view, recent_orders_view
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
+    path('orders/', orders_history_view, name='orders'),
+    path('orders/recent/', recent_orders_view, name='recent_orders'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -14,3 +14,29 @@ def profile_view(request):
         return render(request, 'profile.html', context)
 
     return redirect(reverse("account_login"))
+
+
+def orders_history_view(request):
+    if request.user.is_authenticated:
+
+        form = DetailForm(instance=request.user)
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'orders_history.html', context)
+
+    return redirect(reverse("account_login"))
+
+
+def recent_orders_view(request):
+    if request.user.is_authenticated:
+
+        form = DetailForm(instance=request.user)
+
+        context = {
+            'form': form,
+        }
+        return render(request, 'recent_orders.html', context)
+
+    return redirect(reverse("account_login"))

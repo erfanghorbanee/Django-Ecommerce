5c3c5ba440d769e076e76a932512ecce644c16c6
erfanghorbanee <erfanghorbanee@gmail.com>
2021-10-04 22:51:40 +0330

    Discount code api bug fixed.

--- customers/api/views.py
+++ customers/api/views.py
@@ -11,19 +11,19 @@ utc = pytz.UTC
 
 @api_view(['GET', ])
 def discount_code_view(request):
-    query_set = DiscountCode.objects.all()
-
     code = request.query_params.get('code')
     if code is not None:
-        query_set = query_set.filter(user=request.user, code=code)
+        query_set = DiscountCode.objects.all().filter(user=request.user, code=code)
 
-    serializer = DiscountCodeSerializer(query_set, many=True)
+        serializer = DiscountCodeSerializer(query_set, many=True)
 
-    # check if query_set is not empty.
-    if query_set:
-        # check if discount code has expired.
-        if utc.localize(datetime.now()) > query_set.first().expire_date:
-            return Response({"error": "Sorry, your discount code has expired!"},
-                            status=status.HTTP_404_NOT_FOUND)
+        # check if query_set is not empty.
+        if query_set:
+            # check if discount code has expired.
+            if utc.localize(datetime.now()) > query_set.first().expire_date:
+                return Response({"error": "Sorry, your discount code has expired!"},
+                                status=status.HTTP_406_NOT_ACCEPTABLE)
 
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)

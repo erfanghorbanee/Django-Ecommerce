2a4ae3ff89142979b8861b9880235b8369cb6dfd
erfanghorbanee <erfanghorbanee@gmail.com>
2021-10-06 20:45:23 +0330

    Single product page[navbar] updated.

--- products/views.py
+++ products/views.py
@@ -22,11 +22,9 @@ def products_view(request):
 
 
 def single_product_view(request, product_id):
-    categories = Category.objects.all()
     product = Product.objects.filter(id=product_id).first()
 
     context = {
-        'categories': categories,
         'product': product,
     }
     return render(request, 'products/single_product.html', context)

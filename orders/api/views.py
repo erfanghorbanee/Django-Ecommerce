527468cb03de0e2b869a160ba1052a23967d3a57
erfanghorbanee <erfanghorbanee@gmail.com>
2021-10-07 18:53:27 +0330

    Basket update improved.

--- orders/api/views.py
+++ orders/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from products.models import Product
+
 
 @api_view(['POST', ])
 def update_session(request):
@@ -10,14 +12,22 @@ def update_session(request):
     """
     if request.user.is_authenticated:
         basket = request.session.get('basket')
-        print(basket)   # before
+        print(basket)   # basket before update
 
         product_id = request.POST['product_id']
-        new_quantity = request.POST['new_quantity']
+        product = Product.objects.filter(id=product_id).first()
+
+        new_quantity = int(request.POST['new_quantity'])
+
+        # check if we have enough products in our shop.
+        if new_quantity > product.count:
+            return Response({"error": " متاسفانه این تعداد کالا در انبار موجود نیست."},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
+
         basket[product_id] = new_quantity
         request.session['basket'] = basket
 
-        print(request.session['basket'])  # after
+        print(request.session['basket'])  # basket after update
 
         return Response(status=status.HTTP_200_OK)
 

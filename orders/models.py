bf29183e522150ba2a7a7720f022d6db20e78f2c
erfanghorbanee <erfanghorbanee@gmail.com>
2021-10-11 19:15:56 +0330

    Models updated.

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from customers.models import Address
 from products.models import Product
+from django.utils import timezone
 
 
 class OrderItem(models.Model):
@@ -11,18 +12,14 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
 
-    def get_total_item_price(self):
-        return self.quantity * self.item.price
 
-
-# Basket
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     products = models.ManyToManyField(OrderItem)
     total_price = models.PositiveIntegerField()
-
-    order_date = models.DateTimeField(blank=True, null=True)
+    order_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     ref_code = models.CharField(max_length=10, blank=True, null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
 
     READY_TO_DELIVER = 'آماده ارسال'
     DELIVERING = 'فرستاده شد'
@@ -40,11 +37,4 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.user}"
-
-    def get_total(self):
-        total = 0
-        for product in self.products.all():
-            total += product.get_final_price()
-
-        return total
 

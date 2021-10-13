cf8e917f273f29f2594ce80a05a038ae14b01f4e
erfanghorbanee <erfanghorbanee@gmail.com>
2021-10-13 19:42:52 +0330

    test_models added for customers app.

---
+++ customers/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from customers.models import Customer
+
+
+class ModelsTestCase(TestCase):
+    def test_customer_is_created(self):
+        """Test Customer model"""
+        customer = Customer.objects.create_user(
+            email='erfanghorbanee@gmail.com',
+            first_name='alireza',
+            last_name='ghorbani',
+            phone='+981123123123',
+            password='password'
+        )
+
+        self.assertEqual(str(customer), 'erfanghorbanee@gmail.com')

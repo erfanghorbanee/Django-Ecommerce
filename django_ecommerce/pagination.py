472cb6aab55af02b42c1a5d51f6aa6b2947116a8
erfanghorbanee <erfanghorbanee@gmail.com>
2021-10-14 14:05:25 +0330

    Minor fix.

--- django_ecommerce/pagination.py
+++ django_ecommerce/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 DEFAULT_PAGE = 1
-DEFAULT_PAGE_SIZE = 8
+DEFAULT_PAGE_SIZE = 6
 
 
 class CustomPagination(PageNumberPagination):
